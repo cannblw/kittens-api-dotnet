a7449c4fffaad3142f8f36ddbb41c7e635de4910
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-13 16:40:40 +0100

    Create Cats controller

---
+++ KittensApi/Controllers/CatsController.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using KittensApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KittensApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatsController : ControllerBase
+    {
+        private readonly ILogger<CatsController> _logger;
+        private readonly ICatsService _catsService;
+
+        public CatsController(ILogger<CatsController> logger, ICatsService catsService)
+        {
+            _logger = logger;
+            _catsService = catsService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetUpsideDownCat()
+        {
+            _logger.LogInformation("Getting upside down cat");
+            
+            var image = await _catsService.GetUpsideDownCat();
+
+            if (image == null) return StatusCode((int)HttpStatusCode.InternalServerError);
+            
+            return File(image, MediaTypeNames.Image.Jpeg);
+        }
+    }
+}
