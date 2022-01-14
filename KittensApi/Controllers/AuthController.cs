56c9c0bf670fd53f0417088e0a3bcf938622aeac
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-14 18:52:44 +0100

    Handle registration errors

--- KittensApi/Controllers/AuthController.cs
+++ KittensApi/Controllers/AuthController.cs
@@ -2,8 +2,10 @@ using System.Threading.Tasks;
 using AutoMapper;
 using KittensApi.Dto.Actions;
 using KittensApi.Dto.Details;
+using KittensApi.Exceptions;
 using KittensApi.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace KittensApi.Controllers
 {
@@ -11,13 +13,16 @@ namespace KittensApi.Controllers
     [Route("[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly ILogger _logger;
         private readonly IAuthService _authService;
         private readonly IMapper _mapper;
 
         public AuthController(
+            ILogger<AuthController> logger,
             IAuthService authService,
             IMapper mapper)
         {
+            _logger = logger;
             _authService = authService;
             _mapper = mapper;
         }
@@ -26,11 +31,20 @@ namespace KittensApi.Controllers
         [ProducesResponseType(400)]
         public async Task<ActionResult<RegistrationDetails>> RegisterUser([FromBody] RegisterUserAction action)
         {
-            var (user, token) = await _authService.RegisterUser(action.Email, action.UserName, action.Password);
+            try
+            {
+                _logger.LogInformation("Creating user {UserName}", action.UserName);
 
-            var userDetails = _mapper.Map<UserDetails>(user);
-
-            return new RegistrationDetails(userDetails, token);
+                var (user, token) = await _authService.RegisterUser(action.Email, action.UserName, action.Password);
+                
+                var userDetails = _mapper.Map<UserDetails>(user);
+                return new RegistrationDetails(userDetails, token);
+            }
+            catch (UserAlreadyExistsException ex)
+            {
+                _logger.LogError(ex, "Error creating user {UserName}", action.UserName);
+                throw new KnownErrorException(ex.Message);
+            }
         }
     }
 }
