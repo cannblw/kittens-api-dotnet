5a36e36ae68725e4a4cf9b8348f35c1f69b0b65a
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-14 18:53:17 +0100

    Fix type of Email field in UserDetails

--- KittensApi/Dto/Details/UserDetails.cs
+++ KittensApi/Dto/Details/UserDetails.cs
@@ -3,7 +3,7 @@ namespace KittensApi.Dto.Details
     public class UserDetails
     {
         public string UserName { get; set; }
-        
-        public bool Email { get; set; }
+
+        public string Email { get; set; }
     }
 }
