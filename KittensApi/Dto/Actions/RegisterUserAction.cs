0e163665b3c2a5197e707c8bca0d463871369e3a
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-14 18:30:52 +0100

    Add registration DTOs

---
+++ KittensApi/Dto/Actions/RegisterUserAction.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KittensApi.Dto.Actions
+{
+    public class RegisterUserAction
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+     
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+    }
+}
